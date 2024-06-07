867a53490b3380bada1919e416c6f4ed4665ec63
g.sartori <g.sartori@gmail.com>
2024-06-07 15:16:47 +0200

    Server port/context-path configuration

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Spring Boot Web App Template
 
 1. Start the application (bootRun)
-2. Visit http://localhost:8080/api/v1/message/42
+2. Visit http://localhost:8081/api/v1/message/42
--- src/main/groovy/gpagroup/config/ConfigMessage.groovy
+++ src/main/groovy/gpagroup/config/ConfigMessage.groovy
@@ -1,12 +1,10 @@
 package gpagroup.config
 
 import groovy.transform.CompileStatic
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @CompileStatic
 @Configuration
-@ConfigurationProperties
 class ConfigMessage {
     String text
 }
